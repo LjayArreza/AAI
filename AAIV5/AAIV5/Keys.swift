779255308ed9ac8960f67b9d85eff79c68b2a937
Louie Jay Arreza <ljayarreza@gmail.com>
2020-10-20 14:13:01 +0800

    new update

---
+++ AAIV5/AAIV5/Keys.swift
@@ -0,0 +1,19 @@
+//
+//  Keys.swift
+//  AAIV5
+//
+//  Created by Louie Jay Arreza on 10/20/20.
+//
+
+import Foundation
+
+struct keys {
+    
+    static let apikey = ""
+    static let secretkey = ""
+    static let sdkKeyDemo = "" // ai.advance.liveness.demo
+    static let sdkKeyTest = "" // test.liveness.app
+    static let demoSecretKey = "" // ai.advance.liveness.demo
+    static let testSecretKet = "" // test.liveness.app
+    
+}
